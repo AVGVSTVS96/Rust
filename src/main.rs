439f2e17f668d5324150d1896f8ee82c0f4cc513
AVGVSTVS96 <122117267+AVGVSTVS96@users.noreply.github.com>
2023-01-31 23:35:22 +0000

    Guessing game progress

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,12 @@
 use std::io;
-
-fn main() {
+use std::cmp::Ordering;
+use rand::Rng;
+// somehow, this works!
+fn main() 
+{
     println!("Guess the number!");
 
-    println!("Please input your guess.");
+    let secret_number = rand::thread_rng().gen_range(1..=100);
 
     let mut guess = String::new();
 
@@ -11,5 +14,35 @@ fn main() {
         .read_line(&mut guess)
         .expect("Failed to read line");
 
+    let guess: u32 = guess.trim().parse().expect("Please type a number!");
+
     println!("You guessed: {guess}");
+
+    println!("The secret number is: {secret_number}");
+    
+    loop {
+        println!("Please input your guess.");
+        let mut guess = String::new();
+        
+    io::stdin()
+        .read_line(&mut guess)
+        .expect("Failed to read line");
+
+        let guess: u32 = guess.trim().parse().expect("Error-1!"); // converting the string to int worked!
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => {
+                println!("Too small!");
+                continue; // continue shouldn't be necessary here
+            }
+            Ordering::Greater => {
+                println!("Too big!");
+                continue; // continue shouldn't be necessary here
+            }
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            }
+        }
+    }
 }
