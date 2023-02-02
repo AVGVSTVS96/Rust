f15725641d950bd164cffdc945407dcb0310a3da
AVGVSTVS96 <122117267+AVGVSTVS96@users.noreply.github.com>
2023-02-02 18:41:22 +0000

    V2

---
+++ Python/scraper2.py
@@ -0,0 +1,43 @@
+import requests
+from bs4 import BeautifulSoup
+
+# URL to scrape
+URL = "https://www.example.com/calendar"
+response = requests.get(URL, timeout=10)
+
+# Parse the HTML content of the page
+soup = BeautifulSoup(response.content, "html.parser")
+
+# Find all the table rows
+rows = soup.find_all("tr")
+
+# Create an empty list to store the data
+data = []
+
+# Loop through each row and extract the data
+for row in rows:
+    cols = row.find_all("td")
+    cols = [ele.text.strip() for ele in cols]
+    data.append([ele for ele in cols if ele]) # Get rid of empty values
+
+# Filter out the rows with no data
+data = [row for row in data if row]
+
+# Create a list to store the formatted data
+formatted_data = []
+
+# Loop through each row of data and format it
+for row in data:
+    event = {}
+    event["Setup Time"] = ""
+    event["Start Time"] = row[0]
+    event["Location"] = row[1]
+    event["Event Title/Menu"] = row[2]
+    event["Actual Guests"] = ""
+    event["End Time"] = row[3]
+    event["Tear Down Time"] = ""
+    formatted_data.append(event)
+
+# Print the formatted data
+for event in formatted_data:
+    print(event)
