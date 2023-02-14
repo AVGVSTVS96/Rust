9da3a758b3393b928c5d1953ecf5df96a189a655
AVGVSTVS96 <122117267+AVGVSTVS96@users.noreply.github.com>
2023-02-14 19:17:52 +0000

    fin

--- Python/Course/doorway.py
+++ Python/Course/doorway.py
@@ -1,6 +1,17 @@
 # program to determine if a box can fit through a door, given both dimensions
-# input dimensions as seperate lines, first box dimensions as ABC next door as X and Y
 
-# doorway
+# box dimensions
 A = int(input())
-B =
+B = int(input())
+C = int(input())
+
+# door dimensions
+X = int(input())
+Y = int(input())
+
+# check if box can fit through door
+if A <= X and B <= Y and C <= Y and C <= X:
+    print("The box can be carried")
+else:
+    print("The box cannot be carried")
+    
