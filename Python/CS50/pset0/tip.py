7a24176f35ee91fb62b20533b3b1738c0c854afa
AVGVSTVS96 <122117267+AVGVSTVS96@users.noreply.github.com>
2023-02-13 23:03:48 -0500

    complete last pset0 problem, 5/5 finished

--- Python/CS50/pset0/tip.py
+++ Python/CS50/pset0/tip.py
@@ -2,6 +2,7 @@
 finish two functions
 """
 
+
 def main():
     dollars = dollars_to_float(input("How much was the meal? "))
     percent = percent_to_float(input("What percentage would you like to tip? "))
