387bbd55bf0fd9f268f4a3bafd75c5eba9f3fed7
AVGVSTVS96 <122117267+AVGVSTVS96@users.noreply.github.com>
2023-02-15 22:43:52 +0000

    updated 5/5

--- Python/CS50/pset1/meal.py
+++ Python/CS50/pset1/meal.py
@@ -7,8 +7,16 @@ If its not time for a meal, output nothing.
 
 def main():
     time = input("What time is it? ")
-    print(convert(time))
+    float_time = convert(time)
 
+    if float_time >= 7.0 or float_time <= 8.0:
+        print("breakfast time")
+    elif float_time >= 12.0 or float_time <= 13.0:
+        print("lunch time")
+    elif float_time>= 18.0 or float_time <= 19.0:
+        print("dinner time")
+    else:
+        pass
 
 def convert(time):
     dot_time = time.replace(":", ".")
