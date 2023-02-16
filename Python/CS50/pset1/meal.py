b83670f8a4809d53ef06e7ef5cc8c730e04af203
AVGVSTVS96 <122117267+AVGVSTVS96@users.noreply.github.com>
2023-02-15 22:20:31 -0500

    cleaned up pset1 5/5

--- Python/CS50/pset1/meal.py
+++ Python/CS50/pset1/meal.py
@@ -1,34 +1,28 @@
-"""
-Implement a program that prompts the user for a
-time
-Output whether its breakfast, lunch, or dinner time.
-If its not time for a meal, output nothing.
-"""
+# Implement a program that prompts the user for a time
+# Output whether its breakfast, lunch, or dinner time
+# If not time for a meal, output nothing.
+
 
 def main():
-    """
-    main part of the program
-    """
     time = input("What time is it? ")
     float_time = convert(time)
 
-    if float_time >= 7.0 and float_time <= 8.0:
+    if 7.0 <= float_time <= 8.0:
         print("breakfast time")
-    elif float_time >= 12.0 and float_time <= 13.0:
+    elif 12.0 <= float_time <= 13.0:
         print("lunch time")
-    elif float_time>= 18.0 and float_time <= 19.0:
+    elif 18.0 <= float_time <= 19.0:
         print("dinner time")
     else:
         pass
 
-def convert(time):
-    """
-    converts time to float
-    """
+
+def convert(time):  # converts time to float w/ 1 decimal place
     dot_time = time.replace(":", ".")
     float_time = float(dot_time)
     round_time = round(float_time, 1)
     return round_time
+
 
 if __name__ == "__main__":
     main()
