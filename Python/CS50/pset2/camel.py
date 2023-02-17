057814c21e3520b8426e35154fa6c16e44df37e0
AVGVSTVS96 <bassim5n@gmail.com>
2023-02-16 19:36:59 -0500

    updated pset2 1/5 - camel.py

--- Python/CS50/pset2/camel.py
+++ Python/CS50/pset2/camel.py
@@ -2,5 +2,20 @@
 # for the name of a variable in camel case
 #
 # and outputs the corresponding name in snake case
+import re
 
-camel = input("camelCase")
+
+def main():
+    camel = input("camelCase: ")
+    snake = split_camel(camel)
+    lower_snake = [word.lower() for word in snake]
+    print(lower_snake[0] + lower_snake[1])
+
+
+def split_camel(string):
+    words = re.findall(r'[a-z]+|[A-Z][a-z]*', string)
+    return words
+
+
+if __name__ == "__main__":
+    main()
