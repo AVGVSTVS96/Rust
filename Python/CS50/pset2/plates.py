56aa52db2de210ce5d0ad740eb54994aa2730beb
AVGVSTVS96 <bassim5n@gmail.com>
2023-02-21 13:58:34 -0500

    updated pset2 4/5 - plates.py - still lost

--- Python/CS50/pset2/plates.py
+++ Python/CS50/pset2/plates.py
@@ -5,6 +5,8 @@
     # Numbers must only come at the end
     # No periods, spaces or punctuation
 """
+
+
 # TODO Stop punctuation marks from being allowed
 
 def main():
@@ -16,12 +18,23 @@ def main():
 
 
 def is_valid(s):
-    if s[:2].isalpha() and 2 <= len(s) <= 6:
-        if len(s) > 2 and not s[:-2].isalpha():  # TODO s[:-2] slices last 2 chars off string
-            return False                         # TODO s[-2:] only looks at the last 2 chars
+    if len(s) < 2 or len(s) > 6:
+        return False
+    else:
         return True
-    return False
 
+#    if s[:2].isdigit():
+#        return False
+#    if s.isalnum() and s[-2:].isdigit() or not s.isalnum():
+#        return True
+
+
+""" if s[:2].isalpha() and 2 <= len(s) <= 6:
+    if len(s) > 2 and not s[:-2].isalpha():  
+        return False                        
+    return True
+return False
+"""
 
 if __name__ == '__main__':
     main()
