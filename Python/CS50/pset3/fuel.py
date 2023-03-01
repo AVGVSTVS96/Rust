e7ce4e4a8abb5975ece3acc96870a6684fe2486e
AVGVSTVS96 <bassim5n@gmail.com>
2023-03-01 14:24:48 -0500

    completed pset3 1/4 - fuel.py

---
+++ Python/CS50/pset3/fuel.py
@@ -0,0 +1,34 @@
+"""
+- Prompt user for fraction (x/y)
+- Output percentage as integer
+- If percentage < 1 output E
+- If percentage > 99 output F
+
+# If x or y != integer (or) x > y (or) y == 0, prompt again
+# Handle ValueError and ZeroDivisionError
+"""
+# Get and evaluate input
+while True:
+    try:
+        fuel_fraction = input("Fraction: ")
+        x, y = fraction_split = fuel_fraction.split("/")
+        x = int(x)
+        y = int(y)
+        if y == 0 or x > y:
+            continue
+        else:
+            break
+    except ValueError:
+        print("Input Integers")
+    except ZeroDivisionError:
+        print("Y must not be 0")
+
+
+# Get percentage
+percentage = (x/y) * 100
+if percentage <= 1:
+    print("E")
+elif percentage >= 99:
+    print("F")
+else:
+    print(round(percentage), "%", sep="")
