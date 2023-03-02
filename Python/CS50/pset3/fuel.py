458656c8dc405899a1685b6918a290dd5f735f44
AVGVSTVS96 <122117267+AVGVSTVS96@users.noreply.github.com>
2023-03-02 01:09:03 -0500

    progress pset3 1/4 - fuel.py - added functions

--- Python/CS50/pset3/fuel.py
+++ Python/CS50/pset3/fuel.py
@@ -7,28 +7,42 @@
 # If x or y != integer (or) x > y (or) y == 0, prompt again
 # Handle ValueError and ZeroDivisionError
 """
-# Get and evaluate input
-while True:
-    try:
-        fuel_fraction = input("Fraction: ")
-        x, y = fraction_split = fuel_fraction.split("/")
-        x = int(x)
-        y = int(y)
-        if y == 0 or x > y:
-            continue
-        else:
-            break
-    except ValueError:
-        print("Input Integers")
-    except ZeroDivisionError:
-        print("Y must not be 0")
 
 
-# Get percentage
-percentage = (x/y) * 100
-if percentage <= 1:
-    print("E")
-elif percentage >= 99:
-    print("F")
-else:
-    print(round(percentage), "%", sep="")
+def prompt():
+    global x, y
+    while True:
+        try:
+            fuel_fraction = input("Fraction: ")
+            x, y = fraction_split = fuel_fraction.split("/")
+            x = int(x)
+            y = int(y)
+            if y == 0 or x > y:
+                continue
+            else:
+                break
+        except ValueError:
+            print("Input Integers")
+        except ZeroDivisionError:
+            print("Y must not be 0")
+
+
+def convert_percent():
+    percentage = (x / y) * 100
+    if percentage <= 1:
+        print("E")
+    elif percentage >= 99:
+        print("F")
+    else:
+        print(round(percentage), "%", sep="")
+
+
+def main():
+    # Get and evaluate input
+    prompt()
+    # Get percentage
+    convert_percent()
+
+
+if __name__ == '__main__':
+    main()
